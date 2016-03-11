912dec5ff5fbd9ec70127c447164541d2b4955ac
Rafael Durán Castañeda <rduran@redpoints.com>
2016-03-11 10:45:39 +0100

    Review setup.py and tests setup
    This fixes #10

--- unshortenit/__init__.py
+++ unshortenit/__init__.py
@@ -5,5 +5,7 @@ __author__ = 'Jeff Kehler'
 __email__ = 'jeffrey.kehler@gmail.com'
 __version__ = '0.2.3'
 
-from .base import unshorten
-
+try:
+    from .base import unshorten
+except ImportError:
+    """requests not installed yet, just ignoring it (setup.py command)"""

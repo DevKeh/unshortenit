31d10bdb167adfc133fbcdb69df8cb8cd1c5bb62
munum <michael.chan.is@gmail.com>
2017-04-20 22:21:54 +1000

    Added atominik.com compatibility

--- unshortenit/base.py
+++ unshortenit/base.py
@@ -34,7 +34,7 @@ def find_in_text(regex, text, flags = re.IGNORECASE | re.DOTALL):
 
 class UnshortenIt(object):
 
-    _adfly_regex = r'adf\.ly|q\.gs|j\.gs|u\.bb|ay\.gy'
+    _adfly_regex = r'adf\.ly|q\.gs|j\.gs|u\.bb|ay\.gy|atominik\.com'
     _linkbucks_regex = r'linkbucks\.com|any\.gs|cash4links\.co|cash4files\.co|dyo\.gs|filesonthe\.net|goneviral\.com|megaline\.co|miniurls\.co|qqc\.co|seriousdeals\.net|theseblogs\.com|theseforums\.com|tinylinks\.co|tubeviral\.com|ultrafiles\.net|urlbeat\.net|whackyvidz\.com|yyv\.co'
     _adfocus_regex = r'adfoc\.us'
     _lnxlu_regex = r'lnx\.lu'
